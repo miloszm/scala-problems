eea7452e0a900c7a3ea37385af5b1e942cdbc1d7
muszynskim <MuszynskiM@quindell.com>
2016-08-17 13:40:17 +0100

    more tests for 4.9

--- src/test/scala/ch04test/PathsWithSumFinderTest.scala
+++ src/test/scala/ch04test/PathsWithSumFinderTest.scala
@@ -17,8 +17,19 @@ class PathsWithSumFinderTest extends FunSpec with Matchers with Inside {
       for (e <- list.drop(1)) root.insert(e)
 
       findPaths(root, 13) should contain(List(6,7))
-      findPaths(root, 8) should contain(List(3,5))
+      findPaths(root, 8) should contain allOf (List(3,5), List(8))
       findPaths(root, 8) should contain(List(8))
+    }
+  }
+
+  describe("paths with nodes with negative values") {
+    it("should be found correctly") {
+      val list = List(-3,-5,-1,5,3,2,4,7,6,9,8,10)
+      val root = TreeNode(list(0), None, None)
+      for (e <- list.drop(1)) root.insert(e)
+
+      findPaths(root, -8) should contain(List(-5,-3))
+      findPaths(root, 4) should contain allOf (List(5, -1), List(3, 5, -1, -3), List(4))
     }
   }
 
